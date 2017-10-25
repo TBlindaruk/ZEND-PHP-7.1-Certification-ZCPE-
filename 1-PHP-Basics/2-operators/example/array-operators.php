a7e3b3fdba84cf6e6e818f329f0b3b1f8deef75f
Tania <t.blindaruk@gmail.com>
2017-10-25 20:27:25 +0300

    Operators

---
+++ 1-PHP-Basics/2-operators/example/array-operators.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tetiana
+ * Date: 25.10.17
+ * Time: 20:06
+ */
+
+$comp = ['Apple','Bananna']===['0'=>'Apple','Bananna'];
+
+var_dump($comp); //bool(true)
+
