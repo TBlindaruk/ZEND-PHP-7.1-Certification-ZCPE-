5f5c2b47fbed7894ea15df77585f80aa4e64c480
Tetiana Blindaruk <t.blindaruk@levi9.com>
2017-10-25 12:42:26 +0300

    Iterator

---
+++ 9-Object-Oriented-Programming/12-SPL/3-Interfaces/example/2-Iterator.php
@@ -0,0 +1,40 @@
+<?php
+
+class myIterator implements Iterator {
+    private $position = 0;
+    private $array = array(
+        "firstelement",
+        "secondelement",
+        "lastelement",
+    );
+
+    public function __construct() {
+        $this->position = 0;
+    }
+
+    public function rewind() {
+        $this->position = 0;
+    }
+
+    public function current() {
+        return $this->array[$this->position];
+    }
+
+    public function key() {
+        return $this->position;
+    }
+
+    public function next() {
+        ++$this->position;
+    }
+
+    public function valid() {
+        return isset($this->array[$this->position]);
+    }
+}
+
+$it = new myIterator;
+
+foreach($it as $key => $value) {
+    echo "$key: $value" . PHP_EOL;
+}
