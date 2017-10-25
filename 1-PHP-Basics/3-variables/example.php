c0320ff4e4e4a46e9b97d0827ea7c28f1fb5266e
Tania <t.blindaruk@gmail.com>
2017-10-25 21:07:16 +0300

    Variables

---
+++ 1-PHP-Basics/3-variables/example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tetiana
+ * Date: 25.10.17
+ * Time: 21:02
+ */
+
+define('IA','I Can have a diferance case',true);
+define('NOT_CLEAR','I have a strong case');
+
+echo IA . PHP_EOL;
+echo Ia . PHP_EOL;
+echo NOT_CLEAR . PHP_EOL;
+echo Not_clear . PHP_EOL; // EROR
