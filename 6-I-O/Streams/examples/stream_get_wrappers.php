412aba1515facd91ba1030ff9b4b70c1da826fd9
Tetiana Blindaruk <t.blindaruk@levi9.com>
2017-11-13 11:01:26 +0200

    add images to question.

---
+++ 6-I-O/Streams/examples/stream_get_wrappers.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Array
+ * (
+ * [0] => https
+ * [1] => ftps
+ * [2] => compress.zlib
+ * [3] => php
+ * [4] => file
+ * [5] => glob
+ * [6] => data
+ * [7] => http
+ * [8] => ftp
+ * [9] => phar
+ * )
+ */
+print_r(stream_get_wrappers());
